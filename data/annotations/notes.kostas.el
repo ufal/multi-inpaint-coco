28c95c8604fd13581581de9a57b2820ee3c2fc93
andreiamanea <manea@ufal.mff.cuni.cz>
2026-01-22 12:01:48 +0100

    added more notes and more downloaded annotations after several corrections where the regex lost a few sentences

---
+++ data/annotations/notes.kostas.el
@@ -0,0 +1,13 @@
+Categorized misstakes:
+
+- a. translation quality - relatively poor:
+    - missing of register (didn't capture the context sometimes)
+    - missgendering (there was not agreement between adjectives and nouns in many cases, where we expected it)
+    - misspelling (in some cases).
+
+- b. image quality
+    - I couldn't recognize the image at all: Coco-Inpaint en34, 99, 82, 90, 93, 103, 162, 175, 183, 230, 292 
+
+- c. issues with the language-source
+    - minor spelling mistakes: 96, 110, 151, 177, 185, 192, 201, 202, 240, 242, 268, 299, 314, 316, 338, 339, 348, 432, 454, 502, 509, 525, 572, 502, 508, 677, 719, 769, 761, 859, 904, 988
+
